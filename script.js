4e8e496177a6fe5a3bd7c512ef5bd1465765d9d1
Chloe <chloevision97@gmail.com>
2022-12-16 06:33:27 +0100

    JavaScript Fully functional Missing Ui

--- script.js
+++ script.js
@@ -5,6 +5,10 @@ const read1 = document.querySelector(`#read1`);
 const read2 = document.querySelector(`#read2`);
 const submitBtn = document.querySelector(`#submit`);
 const table = document.querySelector(`table`);
+const form = document.querySelector(`form`);
+const addnewbookBtn = document.querySelector(`#addBook`);
+const grayout = document.querySelector(`#grayout`);
+const closeBtn = document.querySelector(`#close`);
 
 let myLibary = [];
 function book(title, author, pages, read) {
@@ -16,6 +20,13 @@ function book(title, author, pages, read) {
     return `${title}, ${author}, ${pages}, ${read}`;
   };
 }
+const resetForm = function () {
+  titleInput.value = "";
+  authorInput.value = "";
+  pagesInput.value = "";
+  read1.checked = false;
+  read2.checked = false;
+};
 
 const submit = function (e) {
   if (
@@ -25,8 +36,8 @@ const submit = function (e) {
   ) {
     if (read1.checked === true || read2.checked === true) {
       let readStatus = "";
-      if (read1.checked === true) readStatus = "Read";
-      if (read2.checked === true) readStatus = "Not Read Yet";
+      if (read1.checked === true) readStatus = true;
+      if (read2.checked === true) readStatus = false;
       myLibary.push(
         new book(
           titleInput.value,
@@ -36,11 +47,8 @@ const submit = function (e) {
         )
       );
       e.preventDefault();
-      titleInput.value = "";
-      authorInput.value = "";
-      pagesInput.value = "";
-      read1.checked = false;
-      read2.checked = false;
+      resetForm();
+      closeForm();
       drawBooks(myLibary.length);
     }
   }
@@ -57,19 +65,75 @@ const drawBooks = function (e) {
     const authorbook = document.createElement(`td`);
     const pagesbook = document.createElement(`td`);
     const readbook = document.createElement(`td`);
+    const Actions = document.createElement(`td`);
+    const btn = document.createElement("button");
+    const edit = document.createElement("button");
+    edit.innerHTML = `<svg style="width:24px;height:24px" viewBox="0 0 24 24">
+    <path fill="currentColor" d="M21,5C19.89,4.65 18.67,4.5 17.5,4.5C15.55,4.5 13.45,4.9 12,6C10.55,4.9 8.45,4.5 6.5,4.5C4.55,4.5 2.45,4.9 1,6V20.65C1,20.9 1.25,21.15 1.5,21.15C1.6,21.15 1.65,21.1 1.75,21.1C3.1,20.45 5.05,20 6.5,20C8.45,20 10.55,20.4 12,21.5C13.35,20.65 15.8,20 17.5,20C19.15,20 20.85,20.3 22.25,21.05C22.35,21.1 22.4,21.1 22.5,21.1C22.75,21.1 23,20.85 23,20.6V6C22.4,5.55 21.75,5.25 21,5M21,18.5C19.9,18.15 18.7,18 17.5,18C15.8,18 13.35,18.65 12,19.5V8C13.35,7.15 15.8,6.5 17.5,6.5C18.7,6.5 19.9,6.65 21,7V18.5Z" />
+</svg>`;
+    btn.innerHTML = `<svg style="width:24px;height:24px" viewBox="0 0 24 24">
+    <path fill="currentColor" d="M19,4H15.5L14.5,3H9.5L8.5,4H5V6H19M6,19A2,2 0 0,0 8,21H16A2,2 0 0,0 18,19V7H6V19Z" />
+</svg>`;
+    btn.dataset.ident = i;
+    edit.dataset.ident = i;
+    btn.addEventListener("click", deletethis);
+    edit.addEventListener(`click`, toggle);
+    Actions.appendChild(btn);
+    Actions.appendChild(edit);
     titlebook.textContent = myLibary[i].title;
     authorbook.textContent = myLibary[i].author;
     pagesbook.textContent = myLibary[i].pages;
-    readbook.textContent = myLibary[i].read;
+    if (myLibary[i].read) {
+      readbook.textContent = "Read";
+    } else {
+      readbook.textContent = "Not read yet";
+    }
+    // readbook.textContent = myLibary[i].read;
     tr.appendChild(titlebook);
     tr.appendChild(authorbook);
     tr.appendChild(pagesbook);
     tr.appendChild(readbook);
+    tr.appendChild(Actions);
     table.appendChild(tr);
-    console.log(tr);
   }
 };
 
 const remove = function () {
   document.querySelectorAll(`.thebooks`).forEach((e) => e.remove());
 };
+
+const deletethis = function () {
+  myLibary.splice(this.dataset.ident, 1);
+  remove();
+  if (myLibary.length === 0) return;
+  drawBooks(myLibary.length);
+};
+
+const toggle = function () {
+  if (myLibary[this.dataset.ident].read) {
+    myLibary[this.dataset.ident].read = false;
+  } else {
+    myLibary[this.dataset.ident].read = true;
+  }
+  remove();
+  drawBooks(myLibary.length);
+};
+
+const openForm = function () {
+  form.classList.add("active");
+  grayout.classList.add("active");
+};
+
+const closeForm = function () {
+  form.classList.remove("active");
+  grayout.classList.remove("active");
+  //   if (this.id === close) {
+  //     resetForm();
+  //   }
+  if (this.id === undefined) return;
+  resetForm();
+};
+
+addnewbookBtn.addEventListener(`click`, openForm);
+closeBtn.addEventListener("click", closeForm);
+grayout.addEventListener("click", closeForm);
